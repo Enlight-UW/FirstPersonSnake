4bcbc91dead39656d54204916534cbbc8555bd29
Greg Wolf <greg.d.wolf@gmail.com>
2016-02-05 15:27:30 -0600

    Project Start

---
+++ FirstPersonSnake/Assets/Scripts/Follower.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Follower : MonoBehaviour {
+
+    public GameObject leader;
+    private Vector3 offset;
+
+
+    void Start()
+    {
+
+        offset = transform.position - leader.transform.position;
+    }
+
+    void LateUpdate()
+    {
+
+        transform.position = leader.transform.position + offset;
+    }
+}
---
+++ FirstPersonSnake/Assets/Scripts/SnakeHeadController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SnakeHeadController : MonoBehaviour {
+    private Rigidbody rb;
+    public float speed;
+    private Vector3 movement;
+    public GameObject snakePiece;
+    public GameObject snakeHead;
+    public Text loseText;
+    private bool shouldMove = true;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        
+        loseText.text = "";
+
+    }
+
+	void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        movement = new Vector3(moveHorizontal, 0, moveVertical);
+
+        if (shouldMove)
+        {
+            rb.transform.Translate(movement * speed);
+        }
+
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Wall"))
+        {
+            shouldMove = false;
+            loseText.text = "You Died.... Great job bro";
+            snakeHead.SetActive(false);
+
+        }
+    }
+}
