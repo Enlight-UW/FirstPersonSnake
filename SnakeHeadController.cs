9eef7b2cdfb9a12fc04ffe6cb7a0b3ce4130f78c
Greg Wolf <greg.d.wolf@gmail.com>
2016-03-03 17:02:49 -0600

    Semi-Working Follower
    Commented out the y-position lock in the code and it works better now.
    Still there if you guys want to try to get it working, but I don't think
    we need it.

--- SnakeHeadController.cs
+++ SnakeHeadController.cs
@@ -4,7 +4,11 @@ using UnityEngine.UI;
 
 public class SnakeHeadController : MonoBehaviour {
     public float speed;
-    private Vector3 movement;
+    private Vector3 forward = new Vector3(0,0,1);
+	private Vector3 backward = new Vector3(0,0,-1);
+	private Vector3 left = new Vector3(-1,0,0);
+	private Vector3 right = new Vector3(1,0,0);
+	private Vector3 movement;
     
     public GameObject snakePiece;
     public GameObject snakeHead;
@@ -12,7 +16,6 @@ public class SnakeHeadController : MonoBehaviour {
     private GameObject lastMadePiece;
     private int score;
     private CharacterController characterController;
-    public int turnSpeed;
     private Rigidbody rb;
     private Vector3[] leaderPositions = new Vector3[30];
 
@@ -22,36 +25,57 @@ public class SnakeHeadController : MonoBehaviour {
 
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
-
-        snakeHead = GetComponent<GameObject>();
-
-        snakePieces.Add(Instantiate(snakePiece, new Vector3(0, 0.75f, -1.0f), new Quaternion(0, 0, 0, 0)));
-
+		
+        movement = forward;
+        snakePieces.Add(snakePiece);
         loseText.text = "";
         score = 0;
-        for (int i = 0; i < snakePieces.Count; i++)
-        {
-            if (i == 0)
-            {
-                GameObject piece = (GameObject)snakePieces[i];
-                piece.transform.position = snakeHead.transform.position + offset;
-
-            }
-            else
-            {
-                GameObject piece = (GameObject)snakePieces[i];
-                GameObject previousPiece = (GameObject)snakePieces[i - 1];
-                piece.transform.position = previousPiece.transform.position + offset;
-            }
-        }
-
-
     }
 
 	void FixedUpdate()
     {
-        movement = new Vector3(0, 0, 1);
+		
+        if (Input.GetKeyDown("right"))
+        {
+			if (movement == forward)
+			{
+				movement = right;
+			}
+			else if (movement == backward)
+			{
+				movement = left;
+			}
+			else if (movement == left)
+			{
+				movement = forward;
+			}
+			else if (movement == right)
+			{
+				movement = backward;
+			}
+        }
+        else if (Input.GetKeyDown("left"))
+        {
+			if (movement == forward)
+			{
+				movement = left;
+			}
+			else if (movement == backward)
+			{
+				movement = right;
+			}
+			else if (movement == left)
+			{
+				movement = backward;
+			}
+			else if (movement == right)
+			{
+				movement = forward;
+			}
+        }
+		else if(movement != forward && movement != backward && movement != right && movement != left) {
+			movement = forward;
+		}
         transform.Translate(movement * speed);
         
 
@@ -59,12 +83,18 @@ public class SnakeHeadController : MonoBehaviour {
     }
     void Update()
     {
-
+        //Populates the last leader positions for the past 30 frames.
+		Vector3[] newVector = new Vector3[30];
         for (int i = leaderPositions.Length - 1; i > 0; i--)
         {
-            leaderPositions[i] = leaderPositions[i - 1];
+            newVector[i] = leaderPositions[i - 1];
         }
-        leaderPositions[0] = snakeHead.transform.position;
+        newVector[0] = snakeHead.transform.position;
+        //This part is the part that is throwing errors
+		/*for(int i = 0; i < leaderPositions.Length; i++) {
+			newVector[i].y = 0.75f;
+		}*/
+		leaderPositions = newVector;
     }
 
     void OnTriggerEnter(Collider other)
@@ -75,35 +105,37 @@ public class SnakeHeadController : MonoBehaviour {
         {
             
             other.gameObject.SetActive(false);
-            snakePieces.Add(Instantiate(snakePiece,  new Vector3(0,0.75f,-2.0f), new Quaternion(0,0,0,0)));
+            snakePieces.Add((GameObject)Instantiate(snakePiece,  new Vector3(0,0.5f,-2.0f), new Quaternion(0,0,0,0)));
             score++;
         }
-        if (other.gameObject.CompareTag("Wall"))
+        if (other.gameObject.CompareTag("Wall") || other.gameObject.CompareTag("Player"))
         {
             loseText.text = "You Died.... Great job bro";
             
             snakeHead.SetActive(false);
 
         }
+		 
         
     }
     void LateUpdate()
     {
-
+        //Follower 
         for (int i = 0; i < snakePieces.Count; i++)
         {
             if (i == 0)
             {
                 GameObject piece = (GameObject)snakePieces[i];
-                piece.transform.position = leaderPositions[29];
+                piece.transform.position = leaderPositions[3];
 
             }
             else
             {
                 GameObject piece = (GameObject)snakePieces[i];
-                GameObject previousPiece = (GameObject)snakePieces[i - 1];
+                piece.transform.position = leaderPositions[2 * i];
 
             }
         }
+        
     }
 }
