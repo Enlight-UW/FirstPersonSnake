b84d99625c2d5049b5151151c1ce3e4343458799
Greg Wolf <greg.d.wolf@gmail.com>
2016-04-05 19:02:55 -0500

    Scene Switcher Script

---
+++ Enlightsnake/Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour {
+
+	void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            EditorSceneManager.LoadScene(1);
+        }
+        
+    }
+}
